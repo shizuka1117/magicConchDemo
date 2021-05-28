c9dfc23932847046b1d71b1a7132e8fb7432b267
cyh <1098420273@qq.com>
2021-05-28 10:56:09 +0800

    添加了：常用决策的listview模拟显示；点击加号跳转到常用决策的编辑界面

--- app/src/main/java/com/example/magiccouchdemo/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/magiccouchdemo/ui/home/HomeFragment.java
@@ -2,28 +2,73 @@ package com.example.magiccouchdemo.ui.home;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import com.example.magiccouchdemo.R;
+import com.example.magiccouchdemo.databinding.FragmentDashboardBinding;
+import com.example.magiccouchdemo.databinding.FragmentDashboardSetBinding;
+import com.example.magiccouchdemo.databinding.FragmentHomeBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    FragmentHomeBinding binding;
 
+    private List<ComDec> ComDecList = new ArrayList<>();
+
+    //menu获取
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.home_menu,menu);
+    }
+
+    private void initComDec() {
+        for(int i=0;i<10;i++) {
+            ComDec evt1 = new ComDec("What's to eat tonight", "Eating");
+            ComDecList.add(evt1);
+        }
+    }
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, null, false);
+        //绑定fragment_home.xml
+        binding.setLifecycleOwner(getActivity());
+        binding.fab.setOnClickListener(v -> {
+            NavController controller = Navigation.findNavController(v);
+            controller.navigate(R.id.action_navigation_home_to_homeSetFragment);
+        });
 
-        return root;
+        //ListView
+        initComDec();
+        ComDecAdapter adapter = new ComDecAdapter(getActivity(),
+                R.layout.com_dec_data, ComDecList);
+        binding.list1.setAdapter(adapter);
+        return binding.getRoot();
+    }
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        //获取menu resource
+        setHasOptionsMenu(true);
     }
 }
