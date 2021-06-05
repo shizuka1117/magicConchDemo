2b63d8128274c32e153d8efaf5b0819298ea1999
shizuka <2358470826@qq.com>
2021-06-05 14:50:51 +0800

    0.3

--- app/src/main/java/com/example/magiccouchdemo/ui/home/Option.java
+++ app/src/main/java/com/example/magiccouchdemo/ui/home/Option.java
@@ -1,12 +1,40 @@
 package com.example.magiccouchdemo.ui.home;
 
+import java.util.Calendar;
+
 public class Option {
 
     String optionName;
     int value;
+    int times;
+    private Calendar date;
+
+    public int getTimes() {
+        return times;
+    }
+
+    public void setTimes(int times) {
+        this.times = times;
+    }
+
+    public Calendar getDate() {
+        return date;
+    }
+
+    public void setDate(Calendar date) {
+        this.date = date;
+    }
 
     public Option(){
 
+    }
+    public Option (String optionName){
+        this.optionName = optionName;
+    }
+
+    public Option(String name, Calendar date){
+        this.optionName = name;
+        this.date=Calendar.getInstance();
     }
 
     public Option(String optionName, int value) {
