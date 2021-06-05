ee32f7600f83a6312fbee071508813046d7e1f44
cyh <1098420273@qq.com>
2021-06-05 16:26:32 +0800

    ver 0.4

--- app/src/main/java/com/example/magiccouchdemo/ui/home/Home_Page/decisionRecycleAdapter.java
+++ app/src/main/java/com/example/magiccouchdemo/ui/home/Home_Page/decisionRecycleAdapter.java
@@ -3,9 +3,12 @@ package com.example.magiccouchdemo.ui.home.Home_Page;
 import android.annotation.SuppressLint;
 import android.graphics.Color;
 import android.util.Log;
+import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -25,40 +28,52 @@ public class decisionRecycleAdapter extends RecyclerView.Adapter<decisionRecycle
 
     private List<decisionList> dataList = new ArrayList<>();
     //EventViewBinding binding;
+    private boolean isShowCheckBox = false;//表示当前是否是多选状态。
+    private SparseBooleanArray mSelectedPositions = new SparseBooleanArray();//用来存放CheckBox的选中状态，true为选中,false为没有选中
+
 
     public decisionRecycleAdapter(List<decisionList> dataList){
         this.dataList=dataList;
         Log.d("execute","111");
     }
 
+    public interface  onItemClickListener{
+        void onItemClick(CardView view, int position);
+        void  onItemLongClick(CardView view, int position);
+    }
+    //定义了当长按视图中的项目时调用的回调函数的接口。
+    private onItemClickListener onItemClickListener;
+
+    public void setOnItemClickListener(onItemClickListener onItemClickListener){
+        this.onItemClickListener=onItemClickListener;
+    }
+
+
     //内部ViewHolder类
     static class ViewHolder extends RecyclerView.ViewHolder {
-        CardView listView;
+        CardView cardView;
         TextView event_name;
+        CheckBox checkBox;
         ImageView img_id;
         TextView event_tag;
 
-        /**
-         * 设置CardView随机颜色,试了一下，好丑....再改
-         */
-
         randomColor rand = new randomColor();//随机颜色类
-
         Random random = new Random();//随机类
         List<Integer> color_list = rand.getColor();//返回一个color list
-
         Integer r = random.nextInt(rand.getColor().size());//获取随机下标
         Integer ranColor = color_list.get(r);
+
 
         @SuppressLint("ResourceAsColor")
         public ViewHolder(View itemView) {
             super(itemView);
 
             //setBackgroundcolor 无效？但是不能直接setBackgroundcolor(int color)
-            listView = (CardView) itemView;
-            listView.setCardBackgroundColor(Color.parseColor("#"+ranColor.toString()));
+            cardView = (CardView) itemView;
+            cardView.setCardBackgroundColor(Color.parseColor("#"+ranColor.toString()));
 
             event_name = (TextView) itemView.findViewById(R.id.event_name);
+            checkBox = (CheckBox) itemView.findViewById(R.id.checkbox);
             img_id = (ImageView)itemView.findViewById(R.id.tag_img);
             event_tag = (TextView) itemView.findViewById(R.id.event_tag);
         }
@@ -75,20 +90,30 @@ public class decisionRecycleAdapter extends RecyclerView.Adapter<decisionRecycle
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.event_view,parent,false);
         ViewHolder holder = new ViewHolder(view);
         final ViewHolder holder1 = new ViewHolder(view);
-/*
-        holder1.listView.setOnClickListener(new View.OnClickListener() {
+
+        //单击
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int position = holder1.getAdapterPosition()+1;//index>=0
-                decisionList decision = dataList.get(position);
-                Toast.makeText(v.getContext(), "you clicked view " +  decision.getName(),Toast.LENGTH_SHORT).show();
+                int layoutPos=holder.getLayoutPosition();
+                onItemClickListener.onItemClick(holder.cardView,layoutPos);
             }
         });
-*/
+
+        //长按
+        holder.cardView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                int layoutPos=holder.getLayoutPosition();
+                onItemClickListener.onItemClick(holder.cardView,layoutPos);
+                return false;
+            }
+        });
+
         holder.event_name.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int position = holder1.getAdapterPosition()+1;
+                int position = holder.getAdapterPosition()+1;
                 decisionList decision = dataList.get(position);
                 Toast.makeText(v.getContext(), "you clicked Event :" + decision.getName(), Toast.LENGTH_SHORT).show();
             }
@@ -104,16 +129,78 @@ public class decisionRecycleAdapter extends RecyclerView.Adapter<decisionRecycle
      */
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        //判断是否选中
+        boolean itemChecked = isItemChecked(position);
+
+        holder.checkBox.setChecked(itemChecked);
+
+        //checkBox的监听
+        holder.checkBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isItemChecked(position)) {
+                    setItemChecked(position, false);
+                } else {
+                    setItemChecked(position, true);
+                }
+            }
+        });
+
+        //条目view的监听
+         holder.checkBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isItemChecked(position)) {
+                    setItemChecked(position, false);
+                } else {
+                    setItemChecked(position, true);
+                }
+                notifyItemChanged(position);
+            }
+        });
+
         //decisionList list = dataList.get(position);
         holder.event_name.setText(dataList.get(position).getName());
         holder.img_id.setImageResource(dataList.get(position).getImgID());
         holder.event_tag.setText(dataList.get(position).getTag());
     }
 
+
     @Override
     public int getItemCount() {
         return dataList.size();
     }
 
+    //获得选中条目的结果
+    public ArrayList<decisionList> getSelectedItem() {
+        ArrayList<decisionList> selectList = new ArrayList<>();
+        for (int i = 0; i < dataList.size(); i++) {
+            if (isItemChecked(i)) {
+                selectList.add(dataList.get(i));
+            }
+        }
+        return selectList;
+    }
+
+    //设置给定位置条目的选择状态
+    void setItemChecked(int position, boolean isChecked) {
+        if (mSelectedPositions == null) {
+            mSelectedPositions = new SparseBooleanArray();
+        }
+        mSelectedPositions.put(position, isChecked);
+    }
+
+    //根据位置判断条目是否选中
+    private boolean isItemChecked(int position) {
+        if (mSelectedPositions != null) {
+            return mSelectedPositions.get(position);
+        }
+        return false;
+    }
+
+    void setShowCheckBox(boolean check){
+        isShowCheckBox = check;
+    }
 
 }
