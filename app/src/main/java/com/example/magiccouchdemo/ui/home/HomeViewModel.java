bab3ea31f9132152dc5eff29364ba2137cd03106
shizuka <2358470826@qq.com>
2021-06-12 00:21:56 +0800

    0.72 update database

--- app/src/main/java/com/example/magiccouchdemo/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/magiccouchdemo/ui/home/HomeViewModel.java
@@ -5,16 +5,14 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.example.magiccouchdemo.dataBase.Option;
+import com.example.magiccouchdemo.dataBase.Theme;
 
 public class HomeViewModel extends ViewModel {
     public ObservableArrayList<Option> mList = new ObservableArrayList<>();
+    private MutableLiveData<Integer> id = new MutableLiveData<>(-1);
     private MutableLiveData<String> decName = new MutableLiveData<>();
     private MutableLiveData<String> tag = new MutableLiveData<>();
     private String result;
-
-    public HomeViewModel() {
-
-    }
 
     public String getResult() {
         return result;
@@ -22,6 +20,14 @@ public class HomeViewModel extends ViewModel {
 
     public void setResult(String result) {
         this.result = result;
+    }
+
+    public MutableLiveData<Integer> getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id.setValue(id);
     }
 
     public ObservableArrayList<Option> getmList() {
@@ -36,15 +42,22 @@ public class HomeViewModel extends ViewModel {
         return decName;
     }
 
+    public void setDecName(String decName) {
+        this.decName.setValue(decName);
+    }
+
     public MutableLiveData<String> getTag() {
         return tag;
     }
 
-    public void setTag(MutableLiveData<String> tag) {
-        this.tag = tag;
+    public void setTag(String tag) {
+        this.tag.setValue(tag);
     }
 
-    public void setDecName(MutableLiveData<String> decName) {
-        this.decName = decName;
+    public void clear(){
+        this.setmList(new ObservableArrayList<Option>());
+        this.setDecName("");
+        this.setId(-1);
+        this.setTag("");
     }
 }
